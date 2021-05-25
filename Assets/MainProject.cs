5ac9fd237b7f5a0e160331f203a53bee31f33e78
hwjung <hwjung@morai.ai>
2021-05-25 15:45:01 +0900

    first

---
+++ Assets/MainProject.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainProject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 메인 프로젝트
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
